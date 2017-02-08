ff2e7a78debc3c04844d366e2b09e2281acbd418
Will Smith <lol.tihan@gmail.com>
2017-02-08 10:08:51 -0800

    Hooked up Android

--- Droid/MainActivity.cs
+++ Droid/MainActivity.cs
@@ -8,6 +8,10 @@ using Android.Views;
 using Android.Widget;
 using Android.OS;
 
+using Microsoft.Azure.Mobile;
+using Microsoft.Azure.Mobile.Analytics;
+using Microsoft.Azure.Mobile.Crashes;
+
 namespace XamTraining.Droid
 {
 	[Activity(Label = "XamTraining.Droid", Icon = "@drawable/icon", Theme = "@style/MyTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
@@ -21,6 +25,8 @@ namespace XamTraining.Droid
 			base.OnCreate(bundle);
 
 			global::Xamarin.Forms.Forms.Init(this, bundle);
+
+			MobileCenter.Configure("11c31c79-f578-4aeb-b6ca-6d6a5b19dd5c");
 
 			LoadApplication(new App());
 		}
